64b440edc9176744ad43c111f5b2eb3a91ab0c1b
Tor Alver Moen <tor.alver.moen@gmail.com>
2024-06-25 12:07:41 +0200

    Initial commit

---
+++ ColourCodeBreaker/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ColourCodeBreaker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
