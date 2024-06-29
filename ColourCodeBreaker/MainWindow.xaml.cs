fba4a4551d05d21af2ea15d3a2439a18550c028e
Tor Alver Moen <tor.alver.moen@gmail.com>
2024-06-29 11:46:34 +0200

    Created a difficulty settings area
    #jira CB-102

--- ColourCodeBreaker/MainWindow.xaml.cs
+++ ColourCodeBreaker/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ namespace ColourCodeBreaker
 
         int ColourButton = 0; // Button id: Which button was clicked: 0 = none, 1 = Read, 2 = Green, 3 = Yellow, 4 = Orange, 5 = Blue, 6 = White
         int Position = 0;     // Position Id: Which of the four positions to put the currently selected colour
+        int Difficulty = 0;   // 0 = easy (20 turns), 1 = medium (10 turns), 2 = hard (5 turns)
         bool AllowDuplicateColours = false;
         int[] CorrectCombinationIndexes = { 0, 1, 2, 3, 4, 5 };
         bool[] Pos = { false, false, false, false };
@@ -68,6 +69,36 @@ namespace ColourCodeBreaker
                 solution[2].ToString() + ", " + solution[3].ToString();
             // DEBUG END
         }
+
+        private void ChangeDifficulty()
+        {
+            Difficulty++;
+            if (Difficulty > 2)
+            {
+                Difficulty = 0;
+            }
+            DisplayDifficulty();
+        }
+
+        private void DisplayDifficulty()
+        {
+            string tempDifficulty = "";
+            switch (Difficulty)
+            {
+                case 0: tempDifficulty = "Easy"; break;
+                case 1: tempDifficulty = "Medium"; break;
+                case 2: tempDifficulty = "Hard"; break;
+            }
+            if (!IsNewGameStarted)
+            {
+                label_Info.Content = "Sorry but the difficulty cannot be changed mid-game!";
+            }
+            else
+            {
+                labelDisplayDiff.Content = tempDifficulty;
+            }
+        }
+
         }
 
         private void ColourWasChosen()
@@ -171,7 +202,7 @@ namespace ColourCodeBreaker
 
         private void btnDifficulty_Click(object sender, RoutedEventArgs e)
         {
-
+            ChangeDifficulty();
         }
 
         private void btnNewGame_Click(object sender, RoutedEventArgs e)
@@ -182,6 +213,16 @@ namespace ColourCodeBreaker
         private void btnExit_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private void checkBox_DuplicateColours_Checked(object sender, RoutedEventArgs e)
+        {
+            AllowDuplicateColours = true;
+        }
+
+        private void checkBox_DuplicateColours_Unchecked(object sender, RoutedEventArgs e)
+        {
+            AllowDuplicateColours = false;
         }
     }
 }
