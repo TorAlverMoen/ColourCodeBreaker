d19c9ce2241aebd56f8583c1bd99169007d19cfc
Tor Alver Moen <tor.alver.moen@gmail.com>
2024-10-11 10:52:12 +0200

    Added a 'Show history' window
    #jira CB-154

---
+++ ColourCodeBreaker/HistoryView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ColourCodeBreaker
+{
+    /// <summary>
+    /// Interaction logic for HistoryView.xaml
+    /// </summary>
+    public partial class HistoryView : Window
+    {
+        public HistoryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
