4569b4a6c879c96e82333af0742cbf16deded8ad
Tor Alver Moen <tor.alver.moen@gmail.com>
2024-10-18 11:16:33 +0200

    Added a history label setup
    This is the setup for the dynamic player guess history view
    
    #jira CB-166

--- ColourCodeBreaker/HistoryView.xaml.cs
+++ ColourCodeBreaker/HistoryView.xaml.cs
@@ -13,5 +13,25 @@ namespace ColourCodeBreaker
         {
             InitializeComponent();
         }
+
+        static Brush[] Colours = [Brushes.Red, Brushes.Green, Brushes.Yellow, Brushes.Orange, Brushes.Blue, Brushes.White];
+        static Brush[] Feedback = [Brushes.Black, Brushes.White];
+        static int colourIndex = 0;
+        static int feedbackIndex = 0;
+        static double positionLeft = 40;
+        static double positionTop = 100;
+        static double width = 25;
+        static double height = 25;
+
+        Label historyLabel = new Label
+        {
+            Content = "",
+            Margin = new Thickness(positionLeft, positionTop, 0, 0),
+            HorizontalAlignment = HorizontalAlignment.Left,
+            VerticalAlignment = VerticalAlignment.Top,
+            Width = width,
+            Height = height,
+            Background = Colours[colourIndex]
+        };
     }
 }
